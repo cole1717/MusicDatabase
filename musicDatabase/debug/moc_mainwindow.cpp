3f8778050f90771fffe868fb87a685a2777f4bdd
tmccarthy33 <mccarthy.ty@husky.neu.edu>
2017-04-14 00:29:08 -0400

    checked out new branch

--- musicDatabase/debug/moc_mainwindow.cpp
+++ musicDatabase/debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[13];
-    char stringdata0[233];
+    QByteArrayData data[16];
+    char stringdata0[289];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,18 +39,23 @@ QT_MOC_LITERAL(6, 83, 13), // "playlist_name"
 QT_MOC_LITERAL(7, 97, 13), // "edit_playlist"
 QT_MOC_LITERAL(8, 111, 15), // "delete_playlist"
 QT_MOC_LITERAL(9, 127, 14), // "playlist_index"
-QT_MOC_LITERAL(10, 142, 28), // "on_playlistAddButton_clicked"
-QT_MOC_LITERAL(11, 171, 31), // "on_playlistDeleteButton_clicked"
-QT_MOC_LITERAL(12, 203, 29) // "on_playlistEditButton_clicked"
+QT_MOC_LITERAL(10, 142, 15), // "add_to_playlist"
+QT_MOC_LITERAL(11, 158, 8), // "track_id"
+QT_MOC_LITERAL(12, 167, 28), // "on_playlistAddButton_clicked"
+QT_MOC_LITERAL(13, 196, 31), // "on_playlistDeleteButton_clicked"
+QT_MOC_LITERAL(14, 228, 29), // "on_playlistEditButton_clicked"
+QT_MOC_LITERAL(15, 258, 30) // "on_addToPlaylistButton_clicked"
 
     },
     "MainWindow\0on_actionExit_triggered\0\0"
     "submit\0on_searchButton_clicked\0"
     "create_playlist\0playlist_name\0"
     "edit_playlist\0delete_playlist\0"
-    "playlist_index\0on_playlistAddButton_clicked\0"
+    "playlist_index\0add_to_playlist\0track_id\0"
+    "on_playlistAddButton_clicked\0"
     "on_playlistDeleteButton_clicked\0"
-    "on_playlistEditButton_clicked"
+    "on_playlistEditButton_clicked\0"
+    "on_addToPlaylistButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -60,7 +65,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       9,   14, // methods
+      11,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -68,15 +73,17 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   59,    2, 0x08 /* Private */,
-       3,    0,   60,    2, 0x08 /* Private */,
-       4,    0,   61,    2, 0x08 /* Private */,
-       5,    1,   62,    2, 0x08 /* Private */,
-       7,    1,   65,    2, 0x08 /* Private */,
-       8,    1,   68,    2, 0x08 /* Private */,
-      10,    0,   71,    2, 0x08 /* Private */,
-      11,    0,   72,    2, 0x08 /* Private */,
-      12,    0,   73,    2, 0x08 /* Private */,
+       1,    0,   69,    2, 0x08 /* Private */,
+       3,    0,   70,    2, 0x08 /* Private */,
+       4,    0,   71,    2, 0x08 /* Private */,
+       5,    1,   72,    2, 0x08 /* Private */,
+       7,    1,   75,    2, 0x08 /* Private */,
+       8,    1,   78,    2, 0x08 /* Private */,
+      10,    2,   81,    2, 0x08 /* Private */,
+      12,    0,   86,    2, 0x08 /* Private */,
+      13,    0,   87,    2, 0x08 /* Private */,
+      14,    0,   88,    2, 0x08 /* Private */,
+      15,    0,   89,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -85,6 +92,8 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::QString,    6,
     QMetaType::Void, QMetaType::QString,    6,
     QMetaType::Void, QMetaType::QString,    9,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString,    9,   11,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -104,9 +113,11 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->create_playlist((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 4: _t->edit_playlist((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 5: _t->delete_playlist((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 6: _t->on_playlistAddButton_clicked(); break;
-        case 7: _t->on_playlistDeleteButton_clicked(); break;
-        case 8: _t->on_playlistEditButton_clicked(); break;
+        case 6: _t->add_to_playlist((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 7: _t->on_playlistAddButton_clicked(); break;
+        case 8: _t->on_playlistDeleteButton_clicked(); break;
+        case 9: _t->on_playlistEditButton_clicked(); break;
+        case 10: _t->on_addToPlaylistButton_clicked(); break;
         default: ;
         }
     }
@@ -137,13 +148,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 9)
+        if (_id < 11)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 9;
+        _id -= 11;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 9)
+        if (_id < 11)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 9;
+        _id -= 11;
     }
     return _id;
 }
